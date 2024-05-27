83c4feb4bec4cd95acf1d9908a5601b687290734
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-05-27 13:25:24 +0900

    fix: global exception handler 반영으로 try-catch문 삭제

--- src/api/recipe.js
+++ src/api/recipe.js
@@ -1,30 +1,21 @@
 const express = require("express");
-const pool = require("../common/data/dataConfig");
+const pool = require("../common/config/DataConfig");
 const router = express.Router();
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id; // 파라미터에서 food ID를 받아옴
 
-  if (!id) {
-    return res.status(400).json({ message: "Food ID is required" });
-  }
-
-  try {
-    const connection = await pool.getConnection();
-    const selectRecipeQuery = `
-      SELECT recipeOrder, description, image
-      FROM RECIPE
-      WHERE id = ?
-      ORDER BY recipeOrder ASC
+  const connection = await pool.getConnection();
+  const selectRecipeQuery = `
+        SELECT recipeOrder, description, image
+        FROM RECIPE
+        WHERE id = ?
+        ORDER BY recipeOrder ASC
     `;
-    const [recipeDetailList] = await connection.query(selectRecipeQuery, [id]);
-    connection.release();
+  const [recipeDetailList] = await connection.query(selectRecipeQuery, [id]);
+  connection.release();
 
-    res.json(recipeDetailList); // JSON 형식으로 응답 반환
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+  res.json(recipeDetailList); // JSON 형식으로 응답 반환
 });
 
 module.exports = router;
