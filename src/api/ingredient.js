5f71dffb80701a61574b300c29ce44a29bf647d1
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-05-27 11:12:34 +0900

    feat: FOOD테이블에서 foodId를 통해  ingredient를 받아오는 api 추가

---
+++ src/api/ingredient.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const pool = require("../common/data/dataConfig");
+const router = express.Router();
+
+router.get("/food/:foodId", async (req, res) => {
+  const foodId = req.params.foodId; // 파라미터에서 food ID를 받아옴
+
+  if (!foodId) {
+    return res.status(400).send({ message: "Food ID is required" });
+  }
+
+  try {
+    const connection = await pool.getConnection();
+    const selectIngredientQuery = `
+      SELECT ingredient
+      FROM FOOD
+      WHERE food_id = ?
+      ORDER BY recipeOrder ASC
+    `;
+    const [recipeIngredientList] = await connection.query(
+      selectIngredientQuery,
+      [foodId]
+    );
+    connection.release();
+
+    res.json(recipeIngredientList); // JSON 형식으로 응답 반환
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
