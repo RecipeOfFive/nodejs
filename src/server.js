a2bb52991364d22831624557a2e407db21325cd6
sktmdgus1212 <sktmdgus1212@naver.com>
2024-05-28 09:54:04 +0900

    fix: delete origin log

--- src/server.js
+++ src/server.js
@@ -10,11 +10,11 @@ const whitelist = ["http://localhost:3000", process.env.AWS_URL];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       // 만일 whitelist 배열에 origin인자가 있을 경우
       callback(null, true); // cors 허용
     } else {
-      callback(new Error(`Not Allowed Origin! (Origin: ${origin})`)); // cors 비허용
+      callback(new Error(`Not Allowed Origin!`)); // cors 비허용
     }
   },
 };
